1a5a7e8ac19f8b58ae0381ed3917f4c02c3ee830
Dalmo Cirne <dalmo@clarifai.com>
2018-07-05 13:07:12 -0400

    Saveing variables in addition to graph

--- py/fit_curve.py
+++ py/fit_curve.py
@@ -40,7 +40,6 @@ with tf.Session() as sess:
 
     # Run the initializer
     sess.run(init)
-    file_writer = tf.summary.FileWriter('../models/', sess.graph)
 
     # Construct a linear model
     pred = tf.add(tf.multiply(X, W), b, name="pred")
@@ -56,7 +55,11 @@ with tf.Session() as sess:
     #  Note, minimize() knows to modify W and b because Variable objects are trainable=True by default
     optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
 
-# Start training
+    # Create file writer and saver
+    file_writer = tf.summary.FileWriter('../models/', sess.graph)
+    saver = tf.train.Saver()
+
+    # Start training
 
     # Fit all training data
     for epoch in range(training_epochs):
@@ -69,17 +72,19 @@ with tf.Session() as sess:
             print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(c), \
                 "W=", sess.run(W), "b=", sess.run(b))
 
-    file_writer = tf.summary.FileWriter('../models/', sess.graph)
+    # Update info for TensorBoard
+    file_writer.add_summary(tf.Summary());
+    file_writer.flush()
 
     print("Optimization Finished!")
     training_cost = sess.run(cost, feed_dict={X: train_X, Y: train_Y})
     print("Training cost=", training_cost, "W=", sess.run(W), "b=", sess.run(b), '\n')
 
     # Graphic display
-    plt.plot(train_X, train_Y, 'ro', label='Original data')
-    plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label='Fitted line')
-    plt.legend()
-    plt.show()
+    # plt.plot(train_X, train_Y, 'ro', label='Original data')
+    # plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label='Fitted line')
+    # plt.legend()
+    # plt.show()
 
     print("Testing... (Mean square loss Comparison)")
     testing_cost = sess.run(prediction, feed_dict={X: test_X, Y: test_Y})  # same function as cost above
@@ -87,10 +92,13 @@ with tf.Session() as sess:
     # Save the graph
     tf.train.write_graph(sess.graph_def, '../models/', 'fit_curve.pb', as_text=False)
 
+    # Save variables
+    saver.save(sess, '../models/variables')
+
     print("Testing cost=", testing_cost)
     print("Absolute mean square loss difference:", abs(training_cost - testing_cost))
 
-    plt.plot(test_X, test_Y, 'bo', label='Testing data')
-    plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label='Fitted line')
-    plt.legend()
-    plt.show()
+    # plt.plot(test_X, test_Y, 'bo', label='Testing data')
+    # plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label='Fitted line')
+    # plt.legend()
+    # plt.show()
